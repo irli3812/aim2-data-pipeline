1222fcbda6691b4628f3549b0b3a67726f22c750
lbonarrigo <luca.bonarrigo@colorado.edu>
2024-05-01 08:11:29 -0600

    Added event parsing & power analyses
    Added eeglab and xdf folders to same folder for ease of access. Added event parsing and power analyses. Added extra checkpoint for VPN/Wifi

--- eeg_power.m
+++ eeg_power.m
@@ -1,4 +1,4 @@
-%%function [] = eeg_power(subject)%% TRINITY AIM 2 - EEG POWER ANALYSIS        
+%% TRINITY AIM 2 - EEG POWER ANALYSIS        
 % eeg_power.m
 %
 % Purpose: To read in EEG data and conduct power analyses for ONE subject.
@@ -13,14 +13,29 @@
 % Authors: Luca Bonarrigo, Iris Li
 %
 % Created: 3/21/2023
-% Last edited: 4/10/2023
+% Last edited: 5/1/2023
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Housekeeping
 clc; clear; close all;
 set(0,'defaultTextInterpreter', 'latex'); 
 
 [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
-eeglabpath = '.\..\eeglab\'; % INPUT location of eeglab
+eeglabpath = '.\eeglab\'; % INPUT location of eeglab
+addpath('./xdf-Matlab-master');
+
+%% CHECKPOINT
+done = false;
+while(~done)
+    check = input("Are you on CU Boulder VPN or WiFi? (Y/N) ",'s');
+    if(check=='Y')
+        done = true;
+        disp('Loading in EEG data...');
+    else
+        done = false;
+        disp('Once on CU Boulder VPN or WiFi, press any key within the MATLAB terminal to continue.')
+        pause();
+    end
+end
 
 %% 2.1: open XDF and save event values from triggers to a new MATLAB array
 xdfpath = 'Z:\files\MATRIKS\lbonarrigo\SubjectData\xdf\'; 
@@ -111,7 +126,62 @@ while(~done)
 end
 
 %% 2.5: Divide dataset by trials and subtask
+% find indices of nav, robot arm, vs task start and stop
+nav_start = find(strcmp(events.time_series,'Navigation Task Started')==1);
+nav_end = find(strcmp(events.time_series,'Navigation Task Ended')==1);
+arm_start = find(strcmp(events.time_series,'Robotic Arm Task Started')==1);
+arm_end = find(strcmp(events.time_series,'Robotic Arm Task Ended')==1);
+vs_start = find(strcmp(events.time_series,'Observation Task Started')==1);
+vs_end = find(strcmp(events.time_series,'Observation Task Ended')==1);
+rock_select = find(strcmp(events.time_series,'Rock Selected')==1);
+
+% other unused but potentially relevant event triggers:
+% 'Player reached 40 trials'
+% 'Player did not reach proficiency'
+% 'Lvl 0 reached: training video'
+% 'Training video started'
+
+% find actual timestamps associated with all of these events
+% should always be 8 trials
+nav_times = zeros(2,length(nav_end));
+arm_times = zeros(2,length(arm_end));
+vs_times = zeros(2,length(vs_end));
+
+for i=1:length(nav_end)
+    nav_times(1,i) = events.time_stamps(nav_start(i));
+    nav_times(2,i) = events.time_stamps(nav_end(i));
+end
+
+for i=1:length(arm_end)
+    arm_times(1,i) = events.time_stamps(arm_start(i));
+    arm_times(2,i) = events.time_stamps(arm_end(i));
+end
+
+for i=1:length(vs_end)
+    vs_times(1,i) = events.time_stamps(vs_start(i));
+    vs_times(2,i) = events.time_stamps(vs_end(i));
+end
 
 
 %% 2.6: Power Analysis
-%%end
+% run a power analysis for EVERY subtask within each trial (ie, run 24
+% analyses nominally: 3 subtasks x 8 trials)
+
+% TODO: figure out how to save power analyses data as file (not just fig)
+for i=1:length(nav_times)
+    figure; 
+    pop_spectopo(EEG, 1, [nav_times(1,i)      nav_times(2,i)], 'EEG' , 'freq', [3 6 11 20 30], 'freqrange',[1 40],'electrodes','off');
+    eeglab redraw;
+end
+
+for i=1:length(arm_times)
+    figure; 
+    pop_spectopo(EEG, 1, [arm_times(1,i)      arm_times(2,i)], 'EEG' , 'freq', [3 6 11 20 30], 'freqrange',[1 40],'electrodes','off');
+    eeglab redraw;
+end
+
+for i=1:length(vs_times)
+    figure; 
+    pop_spectopo(EEG, 1, [vs_times(1,i)      vs_times(2,i)], 'EEG' , 'freq', [3 6 11 20 30], 'freqrange',[1 40],'electrodes','off');
+    eeglab redraw;
+end
