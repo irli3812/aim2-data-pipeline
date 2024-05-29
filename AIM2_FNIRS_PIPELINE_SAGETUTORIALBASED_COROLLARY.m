3576b4fc956df3ddb84d167b48e1862b48233f16
Iris Li <iris.li@colorado.edu>
2024-05-29 16:37:42 -0600

    corollary fNIRS pipeline

--- AIM2_FNIRS_PIPELINE_SAGETUTORIALBASED_COROLLARY.m
+++ AIM2_FNIRS_PIPELINE_SAGETUTORIALBASED_COROLLARY.m
@@ -1,35 +1,48 @@
-clear;
-clc;
-%% NIRS toolbox must be downloaded and added to directory path for this to work
-toolboxpath = 'C:\Users\Iris Li\Desktop\fNIRS_nback_analysis\nirs-toolbox-2022.4.26';
-if exist(toolboxpath, 'dir')
-    % Generate path string for the toolbox directory and its subfolders
-    toolboxPaths = genpath(toolboxpath);
-    
-    % Add the generated paths to the MATLAB path
-    addpath(toolboxPaths);
-    
-    disp(['NIRS toolbox ', toolboxpath, ' and its subfolders added to the MATLAB path.']);
-else
-    % Display an error message if the folder doesn't exist
-    error(['NIRS toolbox ', toolboxpath, ' does not exist.']);
-end
-%% Working Directory
-% Specify the folder path you want to set as the working directory
-folderPath = 'C:\Users\Iris Li\Desktop\fNIRS_nback_analysis';
-
-% Check if the folder exists
-if exist(folderPath, 'dir')
-    % Set the folder as the working directory
-    cd(folderPath);
-    disp(['Working directory set to: ', folderPath]);
-else
-    % Display an error message if the folder doesn't exist
-    error(['Folder ', folderPath, ' does not exist.']);
-end
-%% load data
-raw = nirs.io.loadDotNirs(fullfile(pwd, "\2023-12-11_001.nirs"));
-%openvar('raw');
+%% TRINITY AIM 2 - fNIRS Data Analysis for Corollary Data
+% AIM2_FNIRS_PIPELINE_SAGETUTORIALBASED
+%
+% Purpose: Analyze a single fNIRS file
+%
+% Inputs: 
+% - location of nirstoolbox
+% - subject number
+% - session number
+%
+% Requirements:
+% - connection to Shuttle Server as Z: drive
+% - *nirs-toolbox-2022.4.26 must be stored in .\..\fNIRS\
+%   - this must also be the working directory
+%
+% Author: Iris Li
+%
+% Created: 3/30/2024
+% Last edited: 4/30/2024
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Housekeeping
+close all; clear all; clc;
+%% Requirement 1: NIRS toolbox must be downloaded and added to directory path for this to work
+toolboxpath = 'C:\Users\Iris Li\Desktop\fNIRS\'; % INPUT location of nirstoolbox* 
+% if exist(toolboxpath, 'dir')
+%     % Generate path string for the toolbox directory and its subfolders
+%     nirstoolbox = genpath(toolboxpath);
+% 
+%     % Set the folder as the working directory
+%     cd(nirstoolbox);
+%     disp(['Working directory set to: ', nirstoolbox]);
+% 
+% else
+%     % Display an error message if the folder doesn't exist
+%     error(['NIRS toolbox path', toolboxpath, ' does not exist.']);
+% end
+%% upload .nirs data file
+nirspath = 'Z:\files\MATRIKS\lbonarrigo\SubjectData\nirs\pilot_nbacks\';
+subno = input('Which subject number (of 3 digits) do you want to run? ', 's'); % for now, this asks for a specific subject number
+session = input('Which session (of 1 digit) of this subject do you want to run? ', 's');
+rawfile = strcat(subno,'_ses-S00',session,'_fnirs.nirs');
+filepath = strcat(nirspath,rawfile);
+% import nirs.io.loadDotNirs.*  <-- in an attempt to fix "Unable to resolve the name nirs.io."
+data = nirs.io.loadDotNirs(filepath); %THIS COMMAND IS NOT WORKING
+openvar('data');
 
 %% Preprocessing Pipeline
 j = nirs.modules.RemoveStimless( );
@@ -37,7 +50,32 @@ j = nirs.modules.RemoveStimless( );
 % Filter Data
 j = eeg.modules.BandPassFilter();
 j.lowpass= .5;
-j.highpass=0.01;
+j.highpass = 0.01;
+
+%% Stimuli
+
+% change the name of stimuli
+j = nirs.modules.RenameStims( j );
+% ASR
+j.listOfChanges = {
+    'stim_channel1', 'First Trial Started';
+    'stim_channel2', 'Task 1 Ended'
+    'stim_channel3', 'Task 2 Ended';
+    'stim_channel4', 'Task 3 Ended';
+    'stim_channel5', 'Next Trial Started';
+    };
+j = nirs.modules.Resample ( j );
+j.Fs = 4;
+
+% IF NEEDED?: Many times you can have files excessive baselines before the start or at
+% the end of an experiment for various reasons.  Here we can trim the data
+% so that there is a max of 30 seconds of pre and post baseline.  This will
+% cut off the time points earlier then 30s before the first stim event in the data and 30s AFTER the last stim event.
+% j = nirs.modules.TrimBaseline( j );
+% j.preBaseline  = 30;
+% j.postBaseline = 30;
+% or j.postBaseline = []; would keep all the data AFTER the last stim event
+% and only discard the pre-event times.
 
 % Before doing regression we must convert to optical density and then
 % hemoglobin.  The two modules must be done in order.
@@ -46,5 +84,11 @@ j = nirs.modules.OpticalDensity( j );
 % Convert to hemoglobin.
 j = nirs.modules.BeerLambertLaw( j );
 
-% Finally, run the pipeline on the raw data and save to anew variable.
-hb = j.run( raw );
+% Finally, run the pipeline on the raw data and save to a new variable.
+hb = j.run( data );
+
+% change stimulus duration, oftentimes the default duration is incorrect to
+% what you setup in psychtoolbox
+% hb = nirs.design.change_stimulus_duration(hb, 'Navigation Task Started', 60);
+% hb = nirs.design.change_stimulus_duration(hb, 'Robotic Arm Task Started', 60);
+% hb = nirs.design.change_stimulus_duration(hb, 'Obsevation Task Started', 60);
