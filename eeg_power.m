de7ff7fa767c329ffd772fb84582a54c0e6d83b8
lbonarrigo <luca.bonarrigo@colorado.edu>
2024-06-14 13:39:10 -0600

    updated eeg_power to use extract_events

--- eeg_power.m
+++ eeg_power.m
@@ -13,14 +13,14 @@
 % Authors: Luca Bonarrigo, Iris Li
 %
 % Created: 3/21/2023
-% Last edited: 5/1/2023
+% Last edited: 6/12/2023
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Housekeeping
 clc; clear; close all;
 set(0,'defaultTextInterpreter', 'latex'); 
 
 [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
-eeglabpath = '.\eeglab\';
+eeglabpath = '.\..\eeglab\'; % INPUT location of eeglab
 addpath('./xdf-Matlab-master');
 
 %% CHECKPOINT
@@ -37,24 +37,18 @@ while(~done)
     end
 end
 
-%% 2.1: open XDF and save event values from triggers to a new MATLAB array
-xdfpath = 'Z:\files\MATRIKS\lbonarrigo\SubjectData\xdf\'; 
-% to modify this to work for multiple subjects, comment the following line:
+%% ask for subject number
 subject = input("Which subject number do you want to run? ",'s'); % for now, this asks for a specific subject number 
-% filename = strcat('sub-P',subject,'_ses-S002_task-Default_run-001_eeg.xdf'); % alter session number here!
-session = input('Which session (of 1 digit) of this subject do you want to run? ', 's');
-filename = strcat('sub-P',subject,'_ses-S00',session,'_task-Default_run-001_eeg.xdf');
-path = strcat(xdfpath,filename);
 
+%% 2.1: open XDF and save event values from triggers to a new MATLAB array
+
+[~,~,events_cor,events_rov,path_rov,path_cor] = extract_events();
+    
 % dataset name (first iteration)
 dataset = strcat('s',subject,'_raw');
 
-% populate raw xdf file in MATLAB (for events)
-xdf = load_xdf(path); % loads into MATLAB
-events = xdf{1,8}; % unity events with times!
-
 % populate raw xdf file in EEGLAB
-EEG = pop_loadxdf(path); 
+EEG = pop_loadxdf(path_rov); 
 [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 0,'setname',dataset,'gui','off'); % save dataset with new name
 
 %% 2.2: pull in and reorder channel locations 
@@ -129,14 +123,22 @@ end
 
 %% 2.5: Divide dataset by trials and subtask
 % find indices of nav, robot arm, vs task start and stop
-nav_start = find(strcmp(events.time_series,'Navigation Task Started')==1);
-nav_end = find(strcmp(events.time_series,'Navigation Task Ended')==1);
-arm_start = find(strcmp(events.time_series,'Robotic Arm Task Started')==1);
-arm_end = find(strcmp(events.time_series,'Robotic Arm Task Ended')==1);
-vs_start = find(strcmp(events.time_series,'Observation Task Started')==1);
-vs_end = find(strcmp(events.time_series,'Observation Task Ended')==1);
-rock_select = find(strcmp(events.time_series,'Rock Selected')==1);
+nav_start = find(strcmp(events_rov.time_series,'Navigation Task Started')==1);
+nav_end = find(strcmp(events_rov.time_series,'Navigation Task Ended')==1);
+arm_start = find(strcmp(events_rov.time_series,'Robotic Arm Task Started')==1);
+arm_end = find(strcmp(events_rov.time_series,'Robotic Arm Task Ended')==1);
+vs_start = find(strcmp(events_rov.time_series,'Observation Task Started')==1);
+vs_end = find(strcmp(events_rov.time_series,'Observation Task Ended')==1);
+rock_select = find(strcmp(events_rov.time_series,'Rock Selected')==1);
 
+% nav end remove duplicates
+for i=2:length(nav_end)
+    if(nav_end(i)-nav_end(i-1)==1)
+        nav_end(i-1)=[];
+    end
+end
+
+%%
 % other unused but potentially relevant event triggers:
 % 'Player reached 40 trials'
 % 'Player did not reach proficiency'
@@ -150,18 +152,21 @@ arm_times = zeros(2,length(arm_end));
 vs_times = zeros(2,length(vs_end));
 
 for i=1:length(nav_end)
-    nav_times(1,i) = events.time_stamps(nav_start(i));
-    nav_times(2,i) = events.time_stamps(nav_end(i));
+    nav_times(1,i) = events_rov.time_stamps(nav_start(i));
+    nav_times(2,i) = events_rov.time_stamps(nav_end(i));
+    nav_dur(i) = nav_times(2,i)-nav_times(1,i);
 end
 
 for i=1:length(arm_end)
-    arm_times(1,i) = events.time_stamps(arm_start(i));
-    arm_times(2,i) = events.time_stamps(arm_end(i));
+    arm_times(1,i) = events_rov.time_stamps(arm_start(i));
+    arm_times(2,i) = events_rov.time_stamps(arm_end(i));
 end
 
+arm_duration = arm_times(2,:)-arm_times(1,:)-23.0756; % 23.0756 = calculated time for robot arm deploy/stowage
+
 for i=1:length(vs_end)
-    vs_times(1,i) = events.time_stamps(vs_start(i));
-    vs_times(2,i) = events.time_stamps(vs_end(i));
+    vs_times(1,i) = events_rov.time_stamps(vs_start(i));
+    vs_times(2,i) = events_rov.time_stamps(vs_end(i));
 end
 
 
@@ -169,21 +174,4 @@ end
 % run a power analysis for EVERY subtask within each trial (ie, run 24
 % analyses nominally: 3 subtasks x 8 trials)
 
-% TODO: figure out how to save power analyses data as file (not just fig)
-for i=1:length(nav_times)
-    figure; 
-    pop_spectopo(EEG, 1, [nav_times(1,i)      nav_times(2,i)], 'EEG' , 'freq', [3 6 11 20 30], 'freqrange',[1 40],'electrodes','off');
-    eeglab redraw;
-end
-
-for i=1:length(arm_times)
-    figure; 
-    pop_spectopo(EEG, 1, [arm_times(1,i)      arm_times(2,i)], 'EEG' , 'freq', [3 6 11 20 30], 'freqrange',[1 40],'electrodes','off');
-    eeglab redraw;
-end
-
-for i=1:length(vs_times)
-    figure; 
-    pop_spectopo(EEG, 1, [vs_times(1,i)      vs_times(2,i)], 'EEG' , 'freq', [3 6 11 20 30], 'freqrange',[1 40],'electrodes','off');
-    eeglab redraw;
-end
+% TO BE FIXED
