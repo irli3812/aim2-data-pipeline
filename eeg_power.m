080d3a32ae34e3d86c67d2ece6a18954ef7cb8ee
lbonarrigo <luca.bonarrigo@colorado.edu>
2024-06-25 07:18:45 -0600

    Add function to split up events times for cor or rover

--- eeg_power.m
+++ eeg_power.m
@@ -13,7 +13,7 @@
 % Authors: Luca Bonarrigo, Iris Li
 %
 % Created: 3/21/2023
-% Last edited: 6/12/2023
+% Last edited: 6/24/2023
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Housekeeping
 clc; clear; close all;
@@ -42,7 +42,7 @@ subject = input("Which subject number do you want to run? ",'s'); % for now, thi
 
 %% 2.1: open XDF and save event values from triggers to a new MATLAB array
 
-[~,~,events_cor,events_rov,path_rov,path_cor] = extract_events();
+[~,~,events_cor,events_rov,path_cor,path_rov] = extract_events(subject);
     
 % dataset name (first iteration)
 dataset = strcat('s',subject,'_raw');
@@ -50,6 +50,11 @@ dataset = strcat('s',subject,'_raw');
 % populate raw xdf file in EEGLAB
 EEG = pop_loadxdf(path_rov); 
 [ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 0,'setname',dataset,'gui','off'); % save dataset with new name
+
+
+%% 2.2: Divide dataset by trials and subtask 
+[nav_times,nav_dur,arm_times,arm_dur,vs_times,vs_dur] = split_times(events_rov,'rov');
+[tct_times,tct_dur,mmr_times,mmr_dur,obs_times,obs_dur] = split_times(events_cor,'cor');
 
 %% 2.2: pull in and reorder channel locations 
 % read default 32 easy channel locations into the xdf file using dipfit
@@ -121,57 +126,29 @@ while(~done)
     end
 end
 
-%% 2.5: Divide dataset by trials and subtask
-% find indices of nav, robot arm, vs task start and stop
-nav_start = find(strcmp(events_rov.time_series,'Navigation Task Started')==1);
-nav_end = find(strcmp(events_rov.time_series,'Navigation Task Ended')==1);
-arm_start = find(strcmp(events_rov.time_series,'Robotic Arm Task Started')==1);
-arm_end = find(strcmp(events_rov.time_series,'Robotic Arm Task Ended')==1);
-vs_start = find(strcmp(events_rov.time_series,'Observation Task Started')==1);
-vs_end = find(strcmp(events_rov.time_series,'Observation Task Ended')==1);
-rock_select = find(strcmp(events_rov.time_series,'Rock Selected')==1);
 
-% nav end remove duplicates
-for i=2:length(nav_end)
-    if(nav_end(i)-nav_end(i-1)==1)
-        nav_end(i-1)=[];
-    end
-end
 
-%%
-% other unused but potentially relevant event triggers:
-% 'Player reached 40 trials'
-% 'Player did not reach proficiency'
-% 'Lvl 0 reached: training video'
-% 'Training video started'
-
-% find actual timestamps associated with all of these events
-% should always be 8 trials
-nav_times = zeros(2,length(nav_end));
-arm_times = zeros(2,length(arm_end));
-vs_times = zeros(2,length(vs_end));
-
-for i=1:length(nav_end)
-    nav_times(1,i) = events_rov.time_stamps(nav_start(i));
-    nav_times(2,i) = events_rov.time_stamps(nav_end(i));
-    nav_dur(i) = nav_times(2,i)-nav_times(1,i);
-end
-
-for i=1:length(arm_end)
-    arm_times(1,i) = events_rov.time_stamps(arm_start(i));
-    arm_times(2,i) = events_rov.time_stamps(arm_end(i));
-end
-
-arm_duration = arm_times(2,:)-arm_times(1,:)-23.0756; % 23.0756 = calculated time for robot arm deploy/stowage
-
-for i=1:length(vs_end)
-    vs_times(1,i) = events_rov.time_stamps(vs_start(i));
-    vs_times(2,i) = events_rov.time_stamps(vs_end(i));
-end
-
+%% 2.6: Extract variable time-length epochs and consolidate data for each task type
 
 %% 2.6: Power Analysis
 % run a power analysis for EVERY subtask within each trial (ie, run 24
 % analyses nominally: 3 subtasks x 8 trials)
 
-% TO BE FIXED
+% TODO: figure out how to save power analyses data as file (not just fig)
+for i=1:length(nav_times)
+    figure; 
+    pop_spectopo(EEG, 1, [nav_times(1,i)      nav_times(2,i)], 'EEG' , 'freq', [3 6 11 20 30], 'freqrange',[1 40],'electrodes','off');
+    eeglab redraw;
+end
+
+for i=1:length(arm_times)
+    figure; 
+    pop_spectopo(EEG, 1, [arm_times(1,i)      arm_times(2,i)], 'EEG' , 'freq', [3 6 11 20 30], 'freqrange',[1 40],'electrodes','off');
+    eeglab redraw;
+end
+
+for i=1:length(vs_times)
+    figure; 
+    pop_spectopo(EEG, 1, [vs_times(1,i)      vs_times(2,i)], 'EEG' , 'freq', [3 6 11 20 30], 'freqrange',[1 40],'electrodes','off');
+    eeglab redraw;
+end
